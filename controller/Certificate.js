349046ca43d17da4b3e9db5b80b64bfd80e22a22
Owolabi adebayo Stephen <owolabiadebayo78@yahoo.com>
2024-05-08 06:21:41 +0100

    Update Certificate.js

--- controller/Certificate.js
+++ controller/Certificate.js
@@ -3,7 +3,7 @@ import Data from "../model/data.js";
 export const fetchCert = async (req, res) => {
   try {
     let users = await Data.find().select(
-      "_id Application_nos Names date Imagedata"
+      "_id Application_nos Names date Imagedata Address1 Gender Phonenos Selected"
     );
     // Sending the fetched users as a response
     return res.status(200).json({ users });
